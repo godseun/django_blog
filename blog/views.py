901bb63cf710c5a7408dd54d544bc5aa4d4e3cfa
kjy <alcy1024@gmail.com>
2021-01-16 11:32:08 +0900

    CBV로 디테일만들기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -10,25 +9,5 @@ class PostList(ListView):
     ordering = '-pk'
 
 
-# def index(request):
-#     posts = Post.objects.all().order_by('-pk')
-#
-#     return render(
-#         request,
-#         'blog/post_list.html',
-#         {
-#             'posts': posts,
-#         }
-#     )
-
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
